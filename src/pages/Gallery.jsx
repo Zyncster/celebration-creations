15942b7566364d5e728abe61266be111f86bec24
Zachary Bishop <zacharytbishop@outlook.com>
2025-12-18 07:17:05 -0600

    Currently creating gallery page; also added 4 images into assets folder for gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,39 @@
 export default function Gallery() {
     return (
-        <section>
-            <h2>Gallery</h2>
-            <p>Showcasing recent balloon arches and garlands. (Photos coming soon)</p>
-        </section>
-    );
+        <div className="gallery-container">
+            <section className="gallery-content">
+                <h1 className="gallery-title">Gallery</h1>
+                <p className="gallery-subtitle">
+                Showcasing recent balloon arches and garlands. (Photos coming soon)
+                </p>
+            </section>
+
+            <section className="image-section">
+                <div className="image-container">
+                    <img src="a" alt="p1" />
+                </div>
+
+                <div className="image-container">
+                    <img src="placeholder2" alt="p2" />
+                </div>
+
+                <div className="image-container">
+                    <img src="placeholder3" alt="p3" />
+                </div>
+
+                <div className="image-container">
+                    <img src="placeholder4" alt="p4" />
+                </div>
+
+                <div className="image-container">
+                    <img src="placeholder5" alt="p5" />
+                </div>
+
+                <div className="image-container">
+                    <img src="placeholder6" alt="p6" />
+                </div>
+            </section>
+
+        </div>
+    )
 }
